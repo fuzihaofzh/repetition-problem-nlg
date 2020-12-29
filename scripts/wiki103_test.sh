732653b840140730d6c4c9cd37e7ba61b8c7c0e2
Zihao Fu <fllubo@qq.com>
2020-12-29 21:37:02 +0800

    Update wiki103_test.sh

--- scripts/wiki103_test.sh
+++ scripts/wiki103_test.sh
@@ -7,16 +7,16 @@
 #SBATCH --partition=infofil01
 GPU=1
 VSIZE=10000
-#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" base
-#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" sampling
+CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" base
+CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" sampling
 CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" temp 
-#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topk10 
-#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topk40 
+CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topk10 
+CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topk40 
 CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topp0.9 
-#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topp0.95 
+CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE" topp0.95 
 
 CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE"_re0.1 temp 
-CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE"_re0.08 temp
+#CUDA_VISIBLE_DEVICES=$GPU ./scripts/wiki103_test_single.sh wiki103_fastbpe_"$VSIZE"_re0.08 temp
 
 
 
