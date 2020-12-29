4fb453f7ef1ae146b23ab9bacd7129a67e3e5112
Zihao Fu <fllubo@qq.com>
2020-12-29 15:06:56 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-pip install fire 
+pip3 install fire 
 g++ -std=c++11 -pthread -O3 tools/fastBPE/fastBPE/main.cc -IfastBPE -o tools/fastBPE/fast
 pip3 install -r tools/e2e-metrics/requirements.txt
 pip3 install --editable tools/fairseq/
