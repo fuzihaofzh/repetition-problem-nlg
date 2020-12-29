39de7fcc98f4bbc71f95abba7d5663d8056e8bfe
Zihao Fu <fllubo@qq.com>
2020-12-29 20:00:42 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-pip3 install nltk fire joblib sacremoses future
+pip3 install nltk fire joblib sacremoses future pipetools
 g++ -std=c++11 -pthread -O3 tools/fastBPE/fastBPE/main.cc -IfastBPE -o tools/fastBPE/fast
 pip3 install -r tools/e2e-metrics/requirements.txt
 pip3 install --editable tools/fairseq/
