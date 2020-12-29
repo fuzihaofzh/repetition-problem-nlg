d538a8924863085a934393e708b3c5e5b6296f75
Zihao Fu <fllubo@qq.com>
2020-12-29 15:43:16 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-pip3 install fire joblib
+pip3 install nltk fire joblib
 g++ -std=c++11 -pthread -O3 tools/fastBPE/fastBPE/main.cc -IfastBPE -o tools/fastBPE/fast
 pip3 install -r tools/e2e-metrics/requirements.txt
 pip3 install --editable tools/fairseq/
